8b36d5243cc72093d83d81e394f4e13a44cc10ae
Antonio_sh <anton@tripod-invest.com>
2019-11-21 15:43:17 +0200

    react try todo + posts

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Rainbow from '../HOC/Rainbow'
+
+const About = ()=> {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A doloremque exercitationem ipsa libero tempore. Animi consectetur delectus dolore illo laboriosam minima nobis nostrum odio reiciendis voluptate! Distinctio eius praesentium quae.</p>
+
+        </div>
+    )
+}
+
+export default Rainbow(About)
---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Contact = (props)=> {
+    // setTimeout(() => {
+    //     props.history.push('/about');
+    // },2000);
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A doloremque exercitationem ipsa libero tempore. Animi consectetur delectus dolore illo laboriosam minima nobis nostrum odio reiciendis voluptate! Distinctio eius praesentium quae.</p>
+
+        </div>
+    )
+}
+
+export default Contact
